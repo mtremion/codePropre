efc825efed34a47e0fba397d7bafac1335053b8d
TREMION Maxime <maxime.tremion@hotmail.fr>
2018-06-12 14:23:22 +0200

    Correction exercice 1

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,24 @@
+package ex1;
+
+/**
+ * @author mtremion
+ *
+ */
+public class Calcul {
+
+	/**
+	 * Méthode prenant 2 entiers en arguments
+	 *  et retournant la somme
+	 */
+	public int addition(int a, int b){
+		return a+b;
+	}
+	
+	/**
+	 * Méthode prenant 2 entiers en arguments
+	 *  et retournant la différence
+	 */
+	public int soustraction(int a, int b){
+		return a-b;
+	}
+}
---
+++ src/main/java/ex1/Entreprise.java
@@ -0,0 +1,47 @@
+package ex1;
+
+import java.util.Date;
+
+/**
+ * @author mtremion
+ *
+ */
+public class Entreprise {
+
+	/**
+	 * siret : int
+	 */
+	public int siret;
+	
+	/**
+	 * nom : String
+	 */
+	public String nom;
+	
+	
+	/**
+	 * adresse : String
+	 */
+	public String adresse;
+	
+	
+	/**
+	 * dateCreation : Date
+	 */
+	public Date dateCreation;
+	
+	/**
+	 * CAPITAL_MAX : int
+	 */
+	public static final int CAPITAL_MAX = 3_000_000;
+	
+	
+	
+	/**
+	 * Méthode pour afficher le calcule
+	 */
+	public void afficherStatut(){
+		
+	}
+	
+}
